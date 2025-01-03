e77c1ad764be2d756fc02bcc64a9b2cdf99dce3e
sharansudheer <saran_sudheer@hotmail.com>
2025-01-03 22:24:33 +0530

    push:Layout
    Login Page Base Layout Completed for testing

---
+++ MiniProject/app/src/main/java/tibesto/apps/school_mgm/ManageStudents.java
@@ -0,0 +1,14 @@
+package tibesto.apps.school_mgm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageStudents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_students);
+    }
+}
