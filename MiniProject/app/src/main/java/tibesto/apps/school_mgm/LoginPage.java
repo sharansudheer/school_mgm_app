7c8ccb68096d631170800b9ad23787d4f6758dc3
sharansudheer <saran_sudheer@hotmail.com>
2025-01-03 23:17:27 +0530

    push:Login-MVC
    Login Page API implementation

--- MiniProject/app/src/main/java/tibesto/apps/school_mgm/LoginPage.java
+++ MiniProject/app/src/main/java/tibesto/apps/school_mgm/LoginPage.java
@@ -3,6 +3,17 @@ package tibesto.apps.school_mgm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +21,11 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        = retrofit.create(.class);
+        Call<> call = .Call("","application/json");
     }
 }
